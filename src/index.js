a31131990cca1a473601ac50dcb0afad25a110ca
Thomas McLaughlin <123tommygun1@gmail.com>
2022-10-17 10:47:33 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { getUsers } from './api';
+
+
+
+
+import {
+  Header
+} from './components';
 
 const App = () => {
+  const [userList, setUserList] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null); 
+
+ // this is new
+  useEffect(() => {
+    getUsers()
+      .then(users => {
+        setUserList(users)
+      })
+      .catch(error => {
+        // something something errors
+      });
+  }, []);
+
   return (
     <div id="App">
-      <h1>Hello, World</h1>
+      <Header userList={ userList } />
     </div>
   );
 }
@@ -13,4 +35,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-

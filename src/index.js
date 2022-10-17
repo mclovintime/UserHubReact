765c2d9c2261b5f44b7769add015446967e89eb6
Leonardo <cruzleonardo831@gmail.com>
2022-10-17 12:44:09 -0500

    update Leo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { getUsers } from './api'
+import { UserPosts } from './components';
 import {
   Header
 } from './components';const App = () => {
+
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
+  const [userTodos, setUserTodos] = useState([]);
+
   const [userList, setUserList] = useState([]);  useEffect(() => {
     getUsers()
       .then(users => {
@@ -12,10 +18,28 @@ import {
       .catch(error => {
         // something something errors
       });
+      
   }, []);  return (
-    <div id='App'>
-      <Header userList={ userList } />
+    // <div id='App'>
+    //   <Header userList={ userList }
+    //    currentUser={ currentUser }
+    //    setCurrentUser={ setCurrentUser } />
+    // </div>
+    <div id="App">
+      <Header
+        userList={ userList }
+        currentUser={ currentUser }
+        setCurrentUser={ setCurrentUser } />
+      {
+        currentUser
+        ? <UserPosts
+            userPosts={ userPosts }
+            currentUser={ currentUser } />
+        : null
+      }
+
     </div>
+
   );
 }
 const element = document.getElementById('root')
